5894c7ad0992f2031d3cd7f3ecf23a74f5f19c4c
rochellebrant <144122520+rochellebrant@users.noreply.github.com>
2024-12-18 12:28:29 +0000

    Update AGT_Chirag_Platform.py

--- bp-bentley-idms/AGT_Chirag_Platform.py
+++ bp-bentley-idms/AGT_Chirag_Platform.py
@@ -6,19 +6,37 @@
 # select * from bentley_apm_reference.pi_tags_chirag_platform where is_active='Y' order by tagname
   
 # %python
-keyVaultScope = 'ZNEUDL1P40INGing-kvl00'
-storageAccount = 'zneudl1pa3apmstore01.blob.core.windows.net'
-storageKeySecret = 'accessKey-bentley-zneudl1pa3apmstore01'
-container = 'data'
-storagePath = 'AZERBAIJAN GEORGIA TURKEY/agt_streaming_chirag_platform/'
-storageKey =  dbutils.secrets.get(scope=keyVaultScope,key=storageKeySecret)
-spark.conf.set("fs.azure.account.key."+storageAccount,storageKey)
-path = "wasbs://"+container+"@"+storageAccount+"/"+storagePath
+KEY_VAULT = 'ZNEUDL1P40INGing-kvl00'
+STORAGE_ACCOUNT = 'zneudl1pa3apmstore01.blob.core.windows.net'
+STORAGE_KEY_SECRET = 'accessKey-bentley-zneudl1pa3apmstore01'
+CONTAINER = 'data'
+STORAGE_PATH = 'AZERBAIJAN GEORGIA TURKEY/agt_streaming_chirag_platform/'
+
+STORAGE_KEY =  dbutils.secrets.get(scope=KEY_VAULT, key=STORAGE_KEY_SECRET)
+spark.conf.set(f"fs.azure.account.key.{STORAGE_ACCOUNT}", STORAGE_KEY)
+path = f"wasbs://{CONTAINER}@{STORAGE_ACCOUNT}/{STORAGE_PATH}"
+
 print(path)
 
-df = spark.sql("select tag_name, ts_utc, tag_value from realtime_agt.vw_agt_streaming_chirag_platform where tag_name in (select tagname from bentley_apm_reference.pi_tags_chirag_platform where is_active='Y') and current_timestamp >= '2020-01-01' and current_timestamp < '2024-09-12' ")
+start_date = '2022-01-01'
+end_date = '2024-09-04'
+
+query = f"""
+SELECT tag_name, ts_utc, tag_value 
+FROM realtime_agt.vw_agt_streaming_chirag_platform 
+WHERE tag_name IN (
+    SELECT tagname 
+    FROM bentley_apm_reference.pi_tags_chirag_platform 
+    WHERE asset = 'Chirag Platform' 
+      AND is_active = 'Y'
+) 
+AND current_timestamp >= '{start_date}' 
+AND current_timestamp < '{end_date}'
+"""
+
+df = spark.sql(query)
 display(df)
 
-if path == 'wasbs://data@zneudl1pa3apmstore01.blob.core.windows.net/AZERBAIJAN GEORGIA TURKEY/agt_streaming_chirag_platform/':
-  # df.coalesce(1).write.format("parquet").mode("overwrite").save(path)
-  # df.coalesce(1).write.format("parquet").mode("append").save(path)
+if STORAGE_PATH == 'wasbs://data@zneudl1pa3apmstore01.blob.core.windows.net/AZERBAIJAN GEORGIA TURKEY/agt_streaming_chirag_platform/':
+  # df.coalesce(1).write.format("parquet").mode("overwrite").save(STORAGE_PATH)
+  # df.coalesce(1).write.format("parquet").mode("append").save(STORAGE_PATH)
